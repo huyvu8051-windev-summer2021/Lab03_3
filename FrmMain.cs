f4d5c59ea277fc14cba9f59ea99d9fb4184269af
huyvu8051 <huyvu8051@gmail.com>
2021-08-28 22:18:49 +0700

    test branch

--- FrmMain.cs
+++ FrmMain.cs
@@ -1,4 +1,5 @@
 ﻿using Lab03_3.Models;
+using Microsoft.Reporting.WinForms;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -18,18 +19,12 @@ namespace Lab03_3
         {
             db = new DBModel();
             InitializeComponent();
-            LoadData();
+            dtpViewByDay.Enabled = true;
+            dtpViewByMonth.Enabled = false;
+            tdpMinDeliveryDate.Enabled = false;
+            tdpMaxDeliveryDate.Enabled = false;
         }
-        void LoadData()
-        {
-            int i = 0;
-            var ls = from s in db.Orders.ToList()
-                     where FilterByDeliveryTime(s.Invoice.DeliveryDate.Date)
-                     select new { STT = i++, s.InvoiceNo, OrderDate = s.Invoice.OrderDate.ToString("dd/MM/yyyy"), DeliveryDate = s.Invoice.DeliveryDate.ToString("dd/MM/yyyy"), SubtotalPrice = s.Quantity * s.Price};
-            dgvDonHang.DataSource = ls.ToList();
-            var tong = ls.Sum(s => s.SubtotalPrice);
-            txtTongCong.Text = tong.ToString();
-        }
+        
 
         private bool FilterByDeliveryTime(DateTime dt)
         {
@@ -48,12 +43,14 @@ namespace Lab03_3
             if (max < min)
             {
                 errorProvider1.SetError(tdpMaxDeliveryDate, "Nhập thời gian lớn hơn field bên trái!");
+                max = min;
+                tdpMaxDeliveryDate.Value = tdpMinDeliveryDate.Value;
             }
         }
 
         void ChangeDeliveryDateFormat()
         {
-            if (chkXemTatCa.Checked)
+            if (rdoViewFromDate.Checked)
             {
                 tdpMinDeliveryDate.Value = tdpMinDeliveryDate.Value.AddDays(-tdpMinDeliveryDate.Value.Day + 1);
                 int maxDayInMonth = DateTime.DaysInMonth(tdpMaxDeliveryDate.Value.Year, tdpMaxDeliveryDate.Value.Month);
@@ -65,13 +62,139 @@ namespace Lab03_3
         private void chkXemTatCa_CheckedChanged(object sender, EventArgs e)
         {
             ChangeDeliveryDateFormat();
-            LoadData();
         }
         private void DateTimePickerValueChange(object sender, EventArgs e)
         {
             ValidateDateTimePicker();
             ChangeDeliveryDateFormat();
-            LoadData();
+        }
+
+        private void FrmMain_Load(object sender, EventArgs e)
+        {
+
+            this.reportViewer1.RefreshReport();
+        }
+
+        private void rdoViewByDate_CheckedChanged(object sender, EventArgs e)
+        {
+            dtpViewByDay.Enabled = true;
+            dtpViewByMonth.Enabled = false;
+            tdpMinDeliveryDate.Enabled = false;
+            tdpMaxDeliveryDate.Enabled = false;
+
+        }
+
+        private void rdoViewByMonth_CheckedChanged(object sender, EventArgs e)
+        {
+            dtpViewByDay.Enabled = false;
+            dtpViewByMonth.Enabled = true;
+            tdpMinDeliveryDate.Enabled = false;
+            tdpMaxDeliveryDate.Enabled = false;
+        }
+
+        private void rdoViewFromDate_CheckedChanged(object sender, EventArgs e)
+        {
+            dtpViewByDay.Enabled = false;
+            dtpViewByMonth.Enabled = false;
+            tdpMinDeliveryDate.Enabled = true;
+            tdpMaxDeliveryDate.Enabled = true;
+        }
+
+        private void btnPrint_Click(object sender, EventArgs e)
+        {
+            this.reportViewer1.Visible = true;
+            if (rdoViewByDay.Checked)
+            {
+                DateTime date = dtpViewByDay.Value;
+                var ls = from s in db.Invoices.ToList()
+                         where s.DeliveryDate.Date == date.Date
+                         select new
+                         {
+                             InvoiceNo = s.InvoiceNo,
+                             DeliveryDate = s.DeliveryDate.ToString("dd/MM/yyyy"),
+                             Subtotal = s.Orders.Sum(a => a.Quantity * a.Price)
+                         };
+                //dgvDonHang.DataSource = ls.ToList();
+
+                
+                ReportParameter[] param = new ReportParameter[] {
+                new ReportParameter("txtDatetime","Ngày " + date.ToString("dd") + " tháng " + date.ToString("MM") + " năm " + date.ToString("yyyy"))
+            };
+                this.reportViewer1.LocalReport.DataSources.Clear();
+
+                this.reportViewer1.LocalReport.ReportPath = "InvoicesReport.rdlc";
+
+                this.reportViewer1.LocalReport.SetParameters(param);
+                var reportResource = new ReportDataSource("DataSetInvoice", ls);
+                this.reportViewer1.LocalReport.DataSources.Add(reportResource);
+                this.reportViewer1.Refresh();
+                if(ls.Count() == 0)
+                {
+                    MessageBox.Show("Không có hoá đơn nào được tìm thấy");
+                }
+            }
+            else if(rdoViewByMonth.Checked)
+            {
+                DateTime date = dtpViewByMonth.Value;
+                var ls = from s in db.Invoices.ToList()
+                         where s.DeliveryDate.Date.Month == date.Month
+                         select new
+                         {
+                             InvoiceNo = s.InvoiceNo,
+                             DeliveryDate = s.DeliveryDate.ToString("dd/MM/yyyy"),
+                             Subtotal = s.Orders.Sum(a => a.Quantity * a.Price)
+                         };
+                //dgvDonHang.DataSource = ls.ToList();
+
+
+                ReportParameter[] param = new ReportParameter[] {
+                new ReportParameter("txtDatetime", "Tháng " + date.ToString("MM") + " năm " + date.ToString("yyyy"))
+            };
+                this.reportViewer1.LocalReport.DataSources.Clear();
+
+                this.reportViewer1.LocalReport.ReportPath = "InvoicesReport.rdlc";
+
+                this.reportViewer1.LocalReport.SetParameters(param);
+                var reportResource = new ReportDataSource("DataSetInvoice", ls);
+                this.reportViewer1.LocalReport.DataSources.Add(reportResource);
+                this.reportViewer1.Refresh();
+                if (ls.Count() == 0)
+                {
+                    MessageBox.Show("Không có hoá đơn nào được tìm thấy");
+                }
+            }
+            else
+            {
+                var ls = from s in db.Invoices.ToList()
+                         where FilterByDeliveryTime(s.DeliveryDate.Date)
+                         select new
+                         {
+                             InvoiceNo = s.InvoiceNo,
+                             DeliveryDate = s.DeliveryDate.ToString("dd/MM/yyyy"),
+                             Subtotal = s.Orders.Sum(a => a.Quantity * a.Price)
+                         };
+                //dgvDonHang.DataSource = ls.ToList();
+
+                DateTime max = tdpMaxDeliveryDate.Value.Date;
+                DateTime min = tdpMinDeliveryDate.Value.Date;
+                ReportParameter[] param = new ReportParameter[] {
+                new ReportParameter("txtDatetime", "Từ " + min.ToString("dd/MM/yyyy") + " đến " + max.ToString("dd/MM/yyyy"))
+            };
+                this.reportViewer1.LocalReport.DataSources.Clear();
+
+                this.reportViewer1.LocalReport.ReportPath = "InvoicesReport.rdlc";
+
+                this.reportViewer1.LocalReport.SetParameters(param);
+                var reportResource = new ReportDataSource("DataSetInvoice", ls);
+                this.reportViewer1.LocalReport.DataSources.Add(reportResource);
+                this.reportViewer1.Refresh();
+                if (ls.Count() == 0)
+                {
+                    MessageBox.Show("Không có hoá đơn nào được tìm thấy");
+                }
+            }// khong lam van có an
+
+            reportViewer1.RefreshReport();
         }
     }
 }
---
+++ Models/InvoiceReportModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab03_3.Models
+{
+    class InvoiceReportModel
+    {
+        public DateTime DeliveryDate { get; set; }
+        public string InvoiceNo { get; set; }
+        public decimal Subtotal { get; set; }
+    }
+}
